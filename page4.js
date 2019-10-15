0cea2ac4261f1d3d83069d5f5cd0550de95cd651
Roms <54295076+Roms2019@users.noreply.github.com>
2019-10-15 15:42:25 -0300

    JQuery - Efeito de slide
    4. No arquivo page4.html, no clique do botão faça com que cada card apareça utilizando efeitos de slide. Um diferente efeito para cada card.

---
+++ page4.js
@@ -0,0 +1,8 @@
+$(function(){
+    
+    $('.btn-primary').click(function(){
+        $(".Tenya").slideDown(1000);
+        $(".Momo").slideDown(5000);
+        $(".Kendo").slideDown(8000);
+    })
+})
